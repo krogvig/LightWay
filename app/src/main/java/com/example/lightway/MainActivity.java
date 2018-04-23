85b8e002114b0fd0456748601fbe15770463e179
MisuMike <38428466+MisuMike@users.noreply.github.com>
2018-04-23 14:18:41 +0200

    Testade att ändra i Mainactivity från android studio

--- app/src/main/java/com/example/lightway/MainActivity.java
+++ app/src/main/java/com/example/lightway/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void testAAAA(){
+        String test = "HelloSirPleaseWork";
+    }
 }
     
