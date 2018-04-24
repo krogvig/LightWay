11545f0939112bb50cf453bbb24895d1550d15f0
robinacm <38428542+robinacm@users.noreply.github.com>
2018-04-24 16:40:15 +0200

    API aktivitet för cykelparkering klass tillagd

--- app/src/main/java/com/example/lightway/MainActivity.java
+++ app/src/main/java/com/example/lightway/MainActivity.java
@@ -40,6 +40,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainAPIActivity.class);
         startActivity(intent);
     }
+
+    public void parkingAPIActivity(View view) {
+        Intent intent = new Intent(this, CykelParkeringAPIActivity.class);
+        startActivity(intent);
+    }
+
 }
 
 
