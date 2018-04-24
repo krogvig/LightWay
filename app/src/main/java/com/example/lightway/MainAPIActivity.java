f1af204e6960c1c0d03d3d81d996ce2ebd66eeba
Felix Forsman <fefo7389@student.su.se>
2018-04-24 12:08:37 +0200

    Gjort om lite för att följa ASync modellen
    Signed-off-by: Felix Forsman <fefo7389@student.su.se>

--- app/src/main/java/com/example/lightway/MainAPIActivity.java
+++ app/src/main/java/com/example/lightway/MainAPIActivity.java
@@ -15,34 +15,16 @@ public class MainAPIActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_api);
-        fetchAPI();
+        callAPI();
     }
 
-    private void fetchAPI () {
-        TextView apiTextView = findViewById(R.id.apiTextView);
-        String result = callAPI();
-        apiTextView.setText(result);      //Update the textView with the result of the API request
-    }
 
-    private String callAPI() {      //Create new thread that can work in the background fetching the API and use the API URL as parameter
+    private void callAPI() {      //Create new thread that can work in the background fetching the API and use the API URL as parameter
         try {
-            URL inputURL = new URL("https://openstreetgs.stockholm.se/geoservice/api/dd0997ea-f66b-4c80-9b87-3783e3faa6f9/wfs?request=GetFeature&typeName=od_gis:Cykelpump_Punkt&cql_filter=Index=%271150056CP%27&outputFormat=JSON")
-            String result = new connectToAPI().doInBackground(inputURL);
-            return result;
+            new connectToAPI().execute(new URL("https://openstreetgs.stockholm.se/geoservice/api/dd0997ea-f66b-4c80-9b87-3783e3faa6f9/wfs?request=GetFeature&typeName=od_gis:Cykelpump_Punkt&cql_filter=Index=%271150056CP%27&outputFormat=JSON"));
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-    }
-
-    private String parse(String jsonLine) {     //To parse the JSON respond as String/Java object (?). Not done.
-        JsonElement jelement = new JsonParser().parse(jsonLine);
-        JsonObject jobject = jelement.getAsJsonObject();
-        jobject = jobject.getAsJsonObject("data");
-        jobject.get("Adress");
-        JsonArray jarray = jobject.getAsJsonArray("translations");
-        jobject = jarray.get(0).getAsJsonObject();
-        String result = jobject.get("translatedText").getAsString();
-        return result;
     }
 }
 
