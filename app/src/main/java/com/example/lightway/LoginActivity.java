01ca752855d1e72f595e70493290a66a8c100d75
robinacm <38428542+robinacm@users.noreply.github.com>
2018-04-26 15:55:48 +0200

    Login/register screens

--- app/src/main/java/com/example/lightway/LoginActivity.java
+++ app/src/main/java/com/example/lightway/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.lightway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText uName = (EditText) findViewById(R.id.username);
+        final EditText uPassword = (EditText) findViewById(R.id.userPassword);
+
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
     }
 }
