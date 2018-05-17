6f7ea9e19cadb5e9f2efe0afcbace61401276f11
Edward <edward.rannelid@gmail.com>
2018-05-17 21:08:05 +0200

    Fixed search bar
    Signed-off-by: Edward <edward.rannelid@gmail.com>

--- app/src/main/java/com/example/lightway/GMapsActivity.java
+++ app/src/main/java/com/example/lightway/GMapsActivity.java
@@ -2,10 +2,13 @@ package com.example.lightway;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
@@ -14,6 +17,13 @@ import android.os.Bundle;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.facebook.login.LoginManager;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -21,6 +31,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.content.pm.PackageManager;
@@ -30,7 +41,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
@@ -94,11 +111,25 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class GMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class GMapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.OnConnectionFailedListener{
 
     private static final String TAG = GMapsActivity.class.getSimpleName();
     private GoogleMap mMap;
     private CameraPosition mCameraPosition;
+
+    // widget for the Searchbar
+    private AutoCompleteTextView mSearchText;
+
+
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40, -168), new LatLng(71, 136));
+
+
+    // adapter object for AutocompleteTextView
+    private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter;
+    private GoogleApiClient mGoogleApiClient;
+    private PlaceInfo mPlace;
 
     // The entry points to the Places API.
     private GeoDataClient mGeoDataClient;
@@ -158,7 +189,10 @@ public class GMapsActivity extends FragmentActivity implements OnMapReadyCallbac
     private DatabaseReference mDatabase;
     private DatabaseReference userToBeRemoved;
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
 
     public void setAllPumps(String key, Pump value) {
         allPumps.put(key, value);
@@ -184,6 +218,9 @@ public class GMapsActivity extends FragmentActivity implements OnMapReadyCallbac
 
         // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_main);
+
+        //
+        mSearchText = (AutoCompleteTextView) findViewById(R.id.input_search);
 
         // Construct a GeoDataClient.
         mGeoDataClient = Places.getGeoDataClient(this, null);
@@ -221,6 +258,66 @@ public class GMapsActivity extends FragmentActivity implements OnMapReadyCallbac
         loadProfileInfo();
 
         // imageFromFirebase = mAuth.getCurrentUser().getPhotoUrl();  //moved to userpoup for now.
+    }
+
+    //
+    private void init(){
+        Log.d(TAG, "init: initializing");
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+
+        mSearchText.setOnItemClickListener(mAutocompleteClickListener);
+
+        mPlaceAutocompleteAdapter = new PlaceAutocompleteAdapter(this, mGeoDataClient,
+                LAT_LNG_BOUNDS, null);
+
+        mSearchText.setAdapter(mPlaceAutocompleteAdapter);
+
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getAction() == KeyEvent.KEYCODE_ENTER){
+                    //execute our method for searching
+                    geoLocate();
+                }
+
+                return false;
+            }
+        });
+
+        hideSoftKeyboard();
+    }
+
+    //searching for a custom location
+    private void geoLocate(){
+        Log.d(TAG, "goeLocate: geolocating");
+
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(GMapsActivity.this);
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }catch (IOException e){
+            Log.e(TAG, "goeLocate: IOExeption: " + e.getMessage() );
+        }
+        if(list.size() > 0){
+            Address address = list.get(0);
+
+            Log.d(TAG, "gorLocate: found a location: " + address.toString());
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM,
+                    address.getAddressLine(0));
+        }
     }
 
     @Override
@@ -431,6 +528,21 @@ public class GMapsActivity extends FragmentActivity implements OnMapReadyCallbac
         }
     }
 
+    //moves camera to searched destination and adds a marker to the location
+    private void moveCamera(LatLng latLng, float zoom, String title){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        mMap.clear();
+
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title(title);
+        mMap.addMarker(options);
+
+        hideSoftKeyboard();
+
+    }
 
     /**
      * Prompts the user for permission to use the device location.
@@ -488,6 +600,7 @@ public class GMapsActivity extends FragmentActivity implements OnMapReadyCallbac
                 Toast.makeText(this, "För att kunna utnyttja appen till fullo behöver du tillåta att den använder din GPS",
                         Toast.LENGTH_LONG).show();
             }
+            init();
         } catch (SecurityException e) {
             Log.e("Exception: %s", e.getMessage());
         }
@@ -499,6 +612,8 @@ public class GMapsActivity extends FragmentActivity implements OnMapReadyCallbac
                 getDeviceLocation();        //Update the location in it's own thread (for better performance) to make sure we're starting from the correct spot
             }
         }).start();
+
+        mMap.clear();
 
         if (allPumps.isEmpty()) {
             Bundle args = new Bundle();
@@ -610,6 +725,8 @@ public class GMapsActivity extends FragmentActivity implements OnMapReadyCallbac
                 getDeviceLocation();        //Update the location in it's own thread (for better performance) to make sure we're starting from the correct spot
             }
         }).start();
+
+        mMap.clear();
 
             if (allParkings.isEmpty()) {
                 Bundle args = new Bundle();
@@ -877,6 +994,86 @@ public class GMapsActivity extends FragmentActivity implements OnMapReadyCallbac
                         }
                     }});
     }
+
+    // Hides the soft keyboard after having pressed enter while searching for a destination
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        InputMethodManager imm = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromInputMethod(mSearchText.getWindowToken(), 0);
+
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager immm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            immm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+    }
+
+    /*
+        ------------------- google places API autocomplete suggestions ----------------------------
+     */
+
+    private AdapterView.OnItemClickListener mAutocompleteClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            hideSoftKeyboard();
+
+            final AutocompletePrediction item = mPlaceAutocompleteAdapter.getItem(i);
+            final String placeId = item.getPlaceId();
+
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+
+        }
+    };
+
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(@NonNull PlaceBuffer places) {
+            if(!places.getStatus().isSuccess()){
+                Log.d(TAG, "onResult: Place query did not complete successfully: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            final Place place = places.get(0);
+
+            try{
+                mPlace = new PlaceInfo();
+                mPlace.setName(place.getName().toString());
+                Log.d(TAG, "onResult: name: " + place.getName());
+//                mPlace.setAttributions(place.getAttributions().toString());
+//                Log.d(TAG, "onResult: attributions: " + place.getAttributions());
+                mPlace.setAddress(place.getAddress().toString());
+                Log.d(TAG, "onResult: address: " + place.getAddress());
+                mPlace.setId(place.getId());
+                Log.d(TAG, "onResult: id: " + place.getId());
+                mPlace.setLatlng(place.getLatLng());
+                Log.d(TAG, "onResult: latlng: " + place.getLatLng());
+                mPlace.setRating(place.getRating());
+                Log.d(TAG, "onResult: rating: " + place.getRating());
+                mPlace.setPhoneNumber(place.getPhoneNumber().toString());
+                Log.d(TAG, "onResult: phone number: " + place.getPhoneNumber());
+                mPlace.setWebsiteURI(place.getWebsiteUri());
+                Log.d(TAG, "onResult: website uri: " + place.getWebsiteUri());
+
+                Log.d(TAG, "onResult: place: " + mPlace.toString());
+            }catch (NullPointerException e){
+                Log.e(TAG, "onResult: NullPointerException: " + e.getMessage());
+            }
+
+//            nedan ifall vi får nullpointers
+//            moveCamera(new LatLng(place.getViewport().getCenter().latitude,
+//                    place.getViewport().getCenter().longitude), DEFAULT_ZOOM, mPlace.getName());
+            moveCamera(mPlace.getLatlng(), DEFAULT_ZOOM, mPlace.getName());
+
+            places.release();
+        }
+    };
+
+
 
 }
 
